f6c6c91759c0daf05f966eab86776921afc7ba6c
Carlos Andres Baena Moncada <pupu@MacBook-Air-de-Carlos.local>
2026-03-01 13:49:43 -0500

    commit 27 - feat: barra de progreso de la meta activa

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// JS Entry
+import './main.scss';
+import Storage from './js/storage.js';
+import { router } from './js/router.js';
+import Dashboard from './js/dashboard.js';
+import Income from './js/income.js';
+import Expenses from './js/expenses.js';
+import Movements from './js/movements.js';
+import Goals from './js/goals.js';
+import { showToast } from './js/utils.js';
+
+// Initialization logic will follow
