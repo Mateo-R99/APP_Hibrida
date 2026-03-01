4c0b6c94fbf89d7337a0b8919c94ca8767d7f7ba
Carlos Andres Baena Moncada <pupu@MacBook-Air-de-Carlos.local>
2026-03-01 17:46:22 -0500

    commit 34 - feat: habilitar compilación Multi-Page Application (MPA) en Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-// Prepared for MPA
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
+        ingresos: resolve(__dirname, 'ingresos.html'),
+        gastos: resolve(__dirname, 'gastos.html'),
+        movimientos: resolve(__dirname, 'movimientos.html'),
+        metas: resolve(__dirname, 'metas.html')
       }
     }
   }
